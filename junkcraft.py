99e73b55733578844d98d75d92e8f3ac688ca838
yuri_kilochek <yuri.kilochek@gmail.com>
2015-03-15 19:25:12 +0300

    Added event handling.

--- junkcraft.py
+++ junkcraft.py
@@ -1,37 +1,23 @@
 # coding=UTF-8
 
-import sys
-import os
-import os.path
-import ctypes
+from engine import *
 
-if sys.platform.startswith('win32'):
-    # assume SDL2.dll is distributed alongside
-    os.environ['PYSDL2_DLL_PATH'] = os.path.dirname(__file__)
 
-# stuff inside is prefixed with 'SDL_' anyway
-from sdl2 import *
+class MyApplication(Application):
+    pass
+
+class MyWindow(Window):
+    def on_key_press(self, key):
+        print('key_press {}'.format(key))
+
+    def on_key_release(self, key):
+        print('key_release {}'.format(key))
+
 
 if __name__ == '__main__':
-    SDL_Init(SDL_INIT_EVERYTHING)
-
-    window = SDL_CreateWindow(
-        b'JunkCraft',
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        800, 600,
-        SDL_WINDOW_RESIZABLE
-    )
-
-    event = SDL_Event()
-    while event:
-        while SDL_PollEvent(ctypes.byref(event)):
-            if event.type == SDL_QUIT:
-                event = None
-                break
-
-    SDL_DestroyWindow(window)
-
-    SDL_Quit()
+    with MyApplication() as application:
+        window = MyWindow(application, title='JunkCraft', size=(800, 600))
+        application.run_event_loop()
 
 
 
