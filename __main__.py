918fd27b896ae2a9f6840fc8afceaf0dc53e0502
yuri_kilochek <yuri.kilochek@gmail.com>
2015-03-22 23:01:07 +0300

    Forgot exports.

--- __main__.py
+++ __main__.py
@@ -4,7 +4,11 @@ from engine import *
 
 if __name__ == "__main__":
     window = Window(title="JunkCraft", size=(800, 600))
+    surface = Surface(window)
     image = Image("resources/leaf.png")
+    render(surface, image)
+    surface.commit()
+
     while True:
         for event in get_more_events():
             print(event)
--- engine/__init__.py
+++ engine/__init__.py
@@ -11,6 +11,10 @@ from ._Surface import *
 from ._Surface import __all__ as all
 __all__ += all
 
+from ._render import *
+from ._render import __all__ as all
+__all__ += all
+
 from ._Window import *
 from ._Window import __all__ as all
 __all__ += all
