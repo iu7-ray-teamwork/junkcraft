1f759c53f53c783c680091a7b6441921c920d77e
yuri_kilochek <yuri.kilochek@gmail.com>
2015-03-22 21:14:21 +0300

    Remove SDL_image in favor of PIL.

--- engine/_Image.py
+++ engine/_Image.py
@@ -1,12 +1,18 @@
 __all__ = []
 
-from ._sdl import *
+from PIL.Image import open as open_image
 
 __all__ += ["Image"]
 class Image:
     def __init__(self, path):
-        pass
+        image = open_image(path).convert("RGBA")
+        self.__size = image.size
+        self.__data = image.tobytes("raw")
 
     @property
     def size(self):
-        return None
+        return self.__size
+
+    @property
+    def data(self):
+        return self.__data
