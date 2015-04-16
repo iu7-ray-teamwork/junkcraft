c2aa91767ae9b45185f838a2a2ba3176514af16d
yuri_kilochek <yuri.kilochek@gmail.com>
2015-04-16 18:08:31 +0300

    Created resize event.

--- __main__.py
+++ __main__.py
@@ -4,15 +4,23 @@ from engine import *
 
 
 if __name__ == "__main__":
+    image = Image("resources/leaf.png")
+
+    def draw_scene(surface):
+        render(surface, image,
+               math.Matrix.rotate(math.radians(45)) *
+               math.Matrix.translate(0.5, 0))
+        surface.commit()
+
     window = Window(title="JunkCraft", size=(800, 600))
     surface = Surface(window)
-    image = Image("resources/leaf.png")
-    render(surface, image, math.Matrix.rotate(
-        math.radians(45)) * math.Matrix.translate(0.5, 0))
-    surface.commit()
+
+    draw_scene(surface)
 
     while True:
         for event in get_more_events():
-            print(event)
-            if type(event) == UserQuitEvent:
+            if event.__class__ == UserQuitEvent:
                 exit()
+            elif event.__class__ == ResizeEvent:
+                if event.window == window:
+                    draw_scene(surface)
--- engine/_events.py
+++ engine/_events.py
@@ -7,6 +7,10 @@ class UserQuitEvent(namedtuple("UserQuitEvent", [])):
     pass
 
 
+class ResizeEvent(namedtuple("ResizeEvent", ["window", "new_size"])):
+    pass
+
+
 class KeyPressEvent(namedtuple("KeyPressEvent", ["window", "key"])):
     pass
 
@@ -14,14 +18,23 @@ class KeyPressEvent(namedtuple("KeyPressEvent", ["window", "key"])):
 class KeyReleaseEvent(namedtuple("KeyReleaseEvent", ["window", "key"])):
     pass
 
+_key_events = {
+    SDL_KEYDOWN: KeyPressEvent,
+    SDL_KEYUP: KeyReleaseEvent,
+}
 
 def get_more_events():
     e = SDL_Event()
     while SDL_PollEvent(e):
         if e.type == SDL_QUIT:
             yield UserQuitEvent()
-        elif (e.type in (SDL_KEYDOWN, SDL_KEYUP) and
-              e.key.windowID in Window._all):
-            event = KeyPressEvent if e.type == SDL_KEYDOWN else KeyReleaseEvent
-            yield event(Window._all[e.key.windowID],
-                        str(SDL_GetKeyName(e.key.keysym.sym), "UTF-8"))
+        elif e.type == SDL_WINDOWEVENT and e.window.windowID in Window._all:
+            window = Window._all[e.window.windowID]
+            if e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED:
+                new_size = e.window.data1, e.window.data2
+                yield ResizeEvent(window, new_size)
+        elif e.type in _key_events and e.key.windowID in Window._all:
+            event = _key_events[e.type]
+            window = Window._all[e.key.windowID]
+            key = str(SDL_GetKeyName(e.key.keysym.sym), "UTF-8")
+            yield event(window, key)
