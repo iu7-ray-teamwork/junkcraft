b24e47de4a4e148b974f177ae0d8f97a859caa33
yuri_kilochek <yuri.kilochek@gmail.com>
2015-04-17 23:08:35 +0300

    Model.contour is now in model space, not image space.

--- engine/_Model.py
+++ engine/_Model.py
@@ -13,10 +13,10 @@ class Model:
         self.__image = Image(os.path.join(os.path.dirname(path), model["image"]))
         self.__size = model["size"]
         self.__density = model["density"]
-        self.__contour = tuple(map(math.Vector, model["contour"]))
+        contour = model["contour"]
         lx, ly = None, None
         hx, hy = None, None
-        for x, y in self.__contour:
+        for x, y in contour:
             if lx is None or x < lx:
                 lx = x
             elif hx is None or x > hx:
@@ -28,6 +28,7 @@ class Model:
         contour_center = math.Vector(hx + lx, hy + ly) / 2
         scale = self.__size / max(hx - lx, hy - ly)
         self.__from_image = math.Matrix.translate(-contour_center) * math.Matrix.scale(+scale, -scale)
+        self.__contour = tuple(map(lambda cp: math.Vector(cp) * self.__from_image, contour))
 
     @property
     def image(self):
