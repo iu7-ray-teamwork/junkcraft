7aa046b3781bea16d66de9a12c3a76aa29364244
yuri_kilochek <yuri.kilochek@gmail.com>
2015-04-17 18:38:46 +0300

    Added texture filtering.

--- engine/_render.py
+++ engine/_render.py
@@ -13,9 +13,10 @@ class _Texture:
         self.size = w, h = size
         self.handle = handle = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, handle)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+        glGenerateMipmap(GL_TEXTURE_2D)
         glBindTexture(GL_TEXTURE_2D, 0)
 
         def cleanup(_):
