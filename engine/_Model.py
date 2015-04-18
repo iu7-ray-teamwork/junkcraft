ba344e576569b2df44b5f3a94f9284dd8c5012fd
yuri_kilochek <yuri.kilochek@gmail.com>
2015-04-18 14:50:32 +0300

    Integrated Chipmunk physics engine.

--- engine/_Model.py
+++ engine/_Model.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import pymunk.util
 
 from ._Image import *
 from . import math
@@ -11,31 +12,41 @@ class Model:
             model = json.load(file)
 
         self.__image = Image(os.path.join(os.path.dirname(path), model["image"]))
-        self.__size = model["size"]
-        self.__density = model["density"]
-        xs, ys = zip(*model["contour"])
+        self.__density = model.get("density", 1.0)
+        self.__elasticity = model.get("elasticity", 0.0)
+        self.__friction = model.get("friction", 0.0)
+
+        shape = model["shape"]
+        xs, ys = zip(*shape)
         lx, ly = min(xs), min(ys)
         hx, hy = max(xs), max(ys)
-        center = math.Vector(hx + lx, hy + ly) / 2
-        scale = self.__size / max(hx - lx, hy - ly)
-        self.__from_image = math.Matrix.translate(-center) * math.Matrix.scale(+scale, -scale)
-        self.__contour = tuple(map(lambda x, y: math.Vector(x, y) * self.__from_image, xs, ys))
+        if "mass_center" not in model:
+            model["mass_center"] = pymunk.util.calc_center(shape)
+        mass_center = math.Vector(model["mass_center"])
+        scale = model["size"] / max(hx - lx, hy - ly)
+        self.__from_image = math.Matrix.translate(-mass_center) * math.Matrix.scale(+scale, -scale)
+
+        self.__shape = tuple(map(lambda x, y: math.Vector(x, y) * self.__from_image, xs, ys))
 
     @property
     def image(self):
         return self.__image
 
     @property
-    def size(self):
-        return self.__size
-
-    @property
     def density(self):
         return self.__density
 
     @property
-    def contour(self):
-        return self.__contour
+    def elasticity(self):
+        return self.__elasticity
+
+    @property
+    def friction(self):
+        return self.__friction
+
+    @property
+    def shape(self):
+        return self.__shape
 
     def render(self, surface, to_surface):
         surface.render_image(self.__image, self.__from_image * to_surface)
--- engine/_Scene.py
+++ engine/_Scene.py
@@ -1,13 +1,23 @@
+import pymunk
+
 from . import math
 
 
 class Scene:
-    def __init__(self, objects=()):
-        self.objects = set(objects)
+    def __init__(self):
+        self.__objects = set()
+        self.__space = pymunk.Space()
+
+    def add(self, object):
+        self.__objects.add(object)
+        self.__space.add(object._body, *object._shapes)
 
     def render(self, surface, viewport):
         size = surface.size
         viewport_to_surface = math.Matrix.scale(max(size) * math.Vector(+0.5, -0.5)) * math.Matrix.translate(size / 2)
         scene_to_surface = ~viewport.to_scene * viewport_to_surface
-        for object in self.objects:
+        for object in self.__objects:
             object.render(surface, scene_to_surface)
+
+    def step(self, time_step):
+        self.__space.step(time_step)
