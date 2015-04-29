18cda11e0c4b5e931fd673c20186ed3d3dfff394
Roman Inflianskas <infroma@gmail.com>
2015-04-29 14:32:58 +0300

    tests

--- tests/engine/math/test_vector.py
+++ tests/engine/math/test_vector.py
@@ -26,15 +26,52 @@ class TestVector:
         assert v.unit == unit
 
     @pytest.mark.parametrize('v1,v2,eq', [
-        (v1, v1, True),
-        (v1, v2, False),
+        (v1[0], v1[0], True),
+        (v1[0], v2[0], False),
     ])
     def test___eq__(self, v1, v2, eq):
-        assert (v1[0] == v2[0]) == eq
+        assert (v1 == v2) == eq
 
     @pytest.mark.parametrize('v1,v2,ne', [
-        (v1, v1, False),
-        (v1, v2, True)
+        (v1[0], v1[0], False),
+        (v1[0], v2[0], True)
     ])
     def test___ne__(self, v1, v2, ne):
-        assert (v1[0] != v2[0]) == ne
+        assert (v1 != v2) == ne
+
+    @pytest.mark.parametrize('v1,v2,mul', [
+        (v1[0], 3, Vector(9, 12)),
+        (v1[0], v2[0], Vector(12, 16))
+    ])
+    def test___mul__(self, v1, v2, mul):
+        assert v1*v2 == mul
+
+    @pytest.mark.parametrize('v1,v2,mul', [
+        (    3, v1[0], Vector(9, 12)),
+        (v2[0], v1[0], Vector(12, 16))
+    ])
+    def test___mul__(self, v1, v2, mul):
+        assert v1*v2 == mul
+
+    @pytest.mark.parametrize('v1,v2,mul', [
+        (v1[0],     3, Vector(9, 12)),
+        (v1[0], v2[0], Vector(12, 16))
+    ])
+    def test___rmul__(self, v1, v2, mul):
+        assert v1*v2 == mul
+
+    @pytest.mark.parametrize('v1,v2,div', [
+        (v1[0],     3, Vector(1, 4/3)),
+        (v1[0], v2[0], Vector(3/4, 1))
+    ])
+    def test___truediv__(self, v1, v2, div):
+        assert v1/v2 == div
+
+    @pytest.mark.parametrize('v1,v2,div', [
+        (v1[0],     3, Vector(1, 1)),
+        (v1[0], v2[0], Vector(0, 1))
+    ])
+    def test___floordiv__(self, v1, v2, div):
+        assert v1//v2 == div
+
+
