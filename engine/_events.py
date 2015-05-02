0cc17fcffd4ac54101e95de4a2e1bd5c0075cb92
yuri_kilochek <yuri.kilochek@gmail.com>
2015-05-02 16:07:31 +0300

    Added mouse button events.

--- engine/_events.py
+++ engine/_events.py
@@ -1,6 +1,7 @@
 import collections
 import weakref
 
+from . import math
 from ._SDL import *
 from ._GL import *
 from ._Window import *
@@ -9,10 +10,23 @@ UserQuitEvent = collections.namedtuple("UserQuitEvent", [])
 ResizeEvent = collections.namedtuple("ResizeEvent", ["window", "new_size"])
 KeyPressEvent = collections.namedtuple("KeyPressEvent", ["window", "key"])
 KeyReleaseEvent = collections.namedtuple("KeyReleaseEvent", ["window", "key"])
+MouseButtonPressEvent = collections.namedtuple("MouseButtonPressEvent", ["window", "position", "button"])
+MouseButtonReleaseEvent = collections.namedtuple("MouseButtonReleaseEvent", ["window", "position", "button"])
 
 _key_events = {
     SDL_KEYDOWN: KeyPressEvent,
     SDL_KEYUP: KeyReleaseEvent,
+}
+
+_mouse_button_events = {
+    SDL_MOUSEBUTTONDOWN: MouseButtonPressEvent,
+    SDL_MOUSEBUTTONUP: MouseButtonReleaseEvent,
+}
+
+_mouse_button_names = {
+    SDL_BUTTON_LEFT: "Left",
+    SDL_BUTTON_MIDDLE: "Wheel",
+    SDL_BUTTON_RIGHT: "Right",
 }
 
 
@@ -35,3 +49,9 @@ def get_more_events():
             window = Window._all[e.key.windowID]
             key = str(SDL_GetKeyName(e.key.keysym.sym), "UTF-8")
             yield event(window, key)
+        elif e.type in _mouse_button_events and e.button.windowID in Window._all:
+            event = _mouse_button_events[e.type]
+            window = Window._all[e.button.windowID]
+            position = math.Vector(e.button.x, e.button.y)
+            button = _mouse_button_names[e.button.button]
+            yield event(window, position, button)
