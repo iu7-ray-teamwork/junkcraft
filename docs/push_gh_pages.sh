929fd3ac7c00153afad389878aa131d34f9b3bf4
Roman Inflianskas <infroma@gmail.com>
2015-05-02 20:11:04 +0300

    Move to docs directory

---
+++ docs/push_gh_pages.sh
@@ -0,0 +1,20 @@
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com/iu7-ray-teamwork/junkcraft.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+cd out
+git init
+git config user.name "Travis CI"
+git config user.email "travis"
+
+for module in "engine" "engine._Surface" "engine.math" "engine.math._Vector" "engine.math._Matrix";
+  do python -m pydoc -w $module;
+done
+
+cp ../index.html index.html
+
+git add .
+git commit -m "Generated docs"
+git push --force --quiet $FULL_REPO master:gh-pages
