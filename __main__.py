e1136e81c94d4e6c182a69c685d886d55ad82501
yuri_kilochek <yuri.kilochek@gmail.com>
2015-05-02 22:05:17 +0300

    Fixed engine import.

--- __main__.py
+++ __main__.py
@@ -2,41 +2,41 @@
 
 import random
 
-from engine import *
+import engine
+
 from _Player import *
 
-
 if __name__ == "__main__":
-    window = Window(title="JunkCraft", size=(800, 600))
-    surface = Surface(window)
+    window = engine.Window(title="JunkCraft", size=(800, 600))
+    surface = engine.Surface(window)
 
-    world = World(damping=0.3)
+    world = engine.World(damping=0.3)
 
     player = Player(world)
 
-    viewport = Viewport(player, scale=10)
+    viewport = engine.Viewport(player, scale=10)
 
-    rocket_model = Model("resources/rocket.json")
+    rocket_model = engine.Model("resources/rocket.json")
     for i in range(random.randint(10, 20)):
-        Object(
+        engine.Object(
             world,
             rocket_model,
             position=(random.uniform(-5, 5), random.uniform(-5, 5)),
-            angle=random.uniform(0, 2 * math.pi),
+            angle=random.uniform(0, 2 * engine.math.pi),
             scale=random.uniform(0.5, 2)
         )
 
     pressed_keys = set()
 
-    for time_step in time_steps(1 / 60):
-        for event in get_more_events():
-            if event.__class__ == UserQuitEvent:
+    for time_step in engine.time_steps(1 / 60):
+        for event in engine.get_more_events():
+            if event.__class__ == engine.UserQuitEvent:
                 exit()
-            elif event.__class__ == KeyPressEvent:
+            elif event.__class__ == engine.KeyPressEvent:
                 pressed_keys.add(event.key)
-            elif event.__class__ == KeyReleaseEvent:
+            elif event.__class__ == engine.KeyReleaseEvent:
                 pressed_keys.discard(event.key)
-            elif event.__class__ == MouseButtonPressEvent:
+            elif event.__class__ == engine.MouseButtonPressEvent:
                 position = event.position * ~viewport.world_to(surface)
                 if event.button == "Left":
                     attach_a_point = position
@@ -45,7 +45,7 @@ if __name__ == "__main__":
                         attach_a_point *= ~attach_a.to_world
                 elif event.button == "Right":
                     detach_a = world.get_object_at(position)
-            elif event.__class__ == MouseButtonReleaseEvent:
+            elif event.__class__ == engine.MouseButtonReleaseEvent:
                 position = event.position * ~viewport.world_to(surface)
                 if event.button == "Left":
                     if attach_a is not None:
@@ -64,16 +64,16 @@ if __name__ == "__main__":
 
         ptw = player.to_world
 
-        o = math.Vector.zero * ptw
+        o = engine.math.Vector.zero * ptw
 
         if "W" in pressed_keys:
-            player.apply_force(math.Vector(0, +force) * ptw - o)
+            player.apply_force(engine.math.Vector(0, +force) * ptw - o)
         if "A" in pressed_keys:
-            player.apply_force(math.Vector(-force, 0) * ptw - o)
+            player.apply_force(engine.math.Vector(-force, 0) * ptw - o)
         if "S" in pressed_keys:
-            player.apply_force(math.Vector(0, -force) * ptw - o)
+            player.apply_force(engine.math.Vector(0, -force) * ptw - o)
         if "D" in pressed_keys:
-            player.apply_force(math.Vector(+force, 0) * ptw - o)
+            player.apply_force(engine.math.Vector(+force, 0) * ptw - o)
 
         world.step(time_step)
 
